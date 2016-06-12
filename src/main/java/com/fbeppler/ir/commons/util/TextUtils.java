9fe33cb3bc1b65c65ca80e1cffed49d74e88f97b
Fabiano D. Beppler <f.beppler@gmail.com>
2016-06-12 11:18:17 -0300

    Added Brazilian stemmer

--- src/main/java/com/fbeppler/ir/commons/util/TextUtils.java
+++ src/main/java/com/fbeppler/ir/commons/util/TextUtils.java
@@ -2,7 +2,12 @@ package com.fbeppler.ir.commons.util;
 
 import org.apache.commons.lang3.StringUtils;
 
+import com.fbeppler.ir.commons.tools.BrazilianStemmer;
+import com.google.common.base.Strings;
+
 public final class TextUtils {
+
+	private static final BrazilianStemmer BRAZILIAN_STEMMER = new BrazilianStemmer();
 
 	private TextUtils() {
 
@@ -10,6 +15,15 @@ public final class TextUtils {
 
 	public static String removeAccents(final String text) {
 		return StringUtils.stripAccents(text);
+	}
+
+	public static String stemmer(final String term) {
+		final String stem = BRAZILIAN_STEMMER.stem(term);
+		if (Strings.isNullOrEmpty(stem)) {
+			return term;
+		}
+
+		return stem;
 	}
 
 }
