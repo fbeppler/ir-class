748a114739b69df576bb632020d3d2c5a8070c48
Fabiano D. Beppler <f.beppler@gmail.com>
2016-06-12 10:56:51 -0300

    Added stopwords

--- src/main/java/com/fbeppler/ir/commons/util/IOUtils.java
+++ src/main/java/com/fbeppler/ir/commons/util/IOUtils.java
@@ -7,7 +7,9 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public final class IOUtils {
@@ -15,11 +17,11 @@ public final class IOUtils {
 	private IOUtils() {
 
 	}
-	
+
 	public static String readFromConsole(final String mensage) throws IOException {
 		System.out.print(mensage);
 		BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
-	    return bufferRead.readLine();
+		return bufferRead.readLine();
 	}
 
 	public static String fromPathToString(final Path path) throws IOException {
@@ -29,6 +31,22 @@ public final class IOUtils {
 	public static List<Path> listTxtFiles(final String folderPath) throws IOException {
 		return Files.list(Paths.get(folderPath)).filter(p -> p.toFile().toString().endsWith("txt"))
 				.collect(Collectors.toList());
+	}
+
+	public static Set<String> stopwords() {
+		try (final BufferedReader reader = new BufferedReader(new InputStreamReader(
+				IOUtils.class.getResourceAsStream("/stopwords-pt.txt"), StandardCharsets.UTF_8))) {
+
+			final Set<String> stopwords = new HashSet<>();
+			String line;
+			while ((line = reader.readLine()) != null) {
+				stopwords.add(TextUtils.removeAccents(line.toLowerCase().trim()));
+			}
+			return stopwords;
+
+		} catch (final IOException e) {
+			throw new IllegalStateException("Can't load stopwords from file");
+		}
 	}
 
 }
